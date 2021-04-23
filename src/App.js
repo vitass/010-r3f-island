9d67d3857bde113094eec2b20695b8028a3a13b6
Stefan Vitasovic <stefanvitasovic@Stefans-MacBook-Pro-16.local>
2021-04-23 15:11:01 +0200

    perf improvements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Suspense } from "react";
-import { EffectComposer, DepthOfField } from "@react-three/postprocessing";
 import { TextureLoader } from "three";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { OrbitControls, Html, Sky } from "@react-three/drei";
@@ -22,13 +21,13 @@ const Terrain = () => {
   );
   return (
     <mesh rotation={[-Math.PI * 0.5, 0, 0]} position={[0, -3, 0]}>
-      <planeBufferGeometry attach="geometry" args={[32, 32, 512, 512]} />
+      <planeBufferGeometry attach="geometry" args={[32, 32, 1024, 1024]} />
       <meshStandardMaterial
         attach="material"
         color="white"
-        metalness={0.05}
         roughness={0.55}
         displacementMap={elevationTexture}
+        displacementScale={1.2}
         normalMap={normalsTexture}
         map={colorsTexture}
       />
@@ -39,29 +38,29 @@ const Terrain = () => {
 function App() {
   return (
     <div className="App">
-      <Canvas colorManagement camera={{ position: [0, 4, -22] }}>
-        <fog attach="fog" args={["#cecece", 10, 70]} />
+      <Canvas
+        // colorManagement
+        concurrent
+        camera={{ position: [30, 2, -30] }}
+        pixelRatio={[1, 1.33]}
+      >
+        {/* <fog attach="fog" args={["#cecece", 10, 70]} /> */}
         <color attach="background" args={["#465ca1"]} />
         <ambientLight intensity={0.2} />
         <pointLight position={[10, 10, 1]} intensity={0.8} />
         <Suspense fallback={<Html center>Loading</Html>}>
           <Terrain />
         </Suspense>
-        <Sky sunPosition={[1, 1, 1]} />
+        <Sky sunPosition={[10, 10, 1]} />
         <OrbitControls
           autoRotate
           minDistance={12}
           maxDistance={20}
           maxPolarAngle={Math.PI * 0.5}
+          enablePan={false}
+          enableRotate={true}
+          enableZoom={false}
         />
-        <EffectComposer>
-          <DepthOfField
-            focusDistance={0}
-            focalLength={0.08}
-            bokehScale={3}
-            height={512}
-          />
-        </EffectComposer>
       </Canvas>
     </div>
   );
